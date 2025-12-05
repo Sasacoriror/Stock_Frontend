1d1533d52db5c871a78ce2bdc1057f2dc4ad9b0e
Sasacoriror <turksacir@hotmail.com>
2025-12-06 00:08:46 +0100

    Added pageable to the frontend where the user can choose between 5, 10, 15 and 20 stocks in the watchlist per page

--- static/Dividend_Tracker/Watchlist/Watchlist.js
+++ static/Dividend_Tracker/Watchlist/Watchlist.js
@@ -10,47 +10,77 @@ document.addEventListener("DOMContentLoaded", () => {
                 });
         });
 
+
+let pageSize = 10;
+let currentPage = 0;
+
+const rowSelect = document.getElementById('rowsPerPage');
+const backBtn = document.getElementById('back-btn');
+const nextBtn = document.getElementById('next-btn');
+const pageInfo = document.getElementById('pageInfo');
+
+rowSelect.addEventListener('change', ()=> {
+    pageSize = parseInt(rowSelect.value);
+    currentPage = 0;
+    fetchWatchlist();
+});
+
+backBtn.addEventListener('click', () => {
+    if (currentPage > 0){
+        currentPage--;
+        fetchWatchlist();
+    }
+});
+
+nextBtn.addEventListener('click', () => {
+    if (!nextBtn.disabled){
+        currentPage++;
+        fetchWatchlist();
+    }
+});
+
 async function fetchWatchlist() {
     try {
-        const response = await fetch(apiUrl);
+        const response = await fetch(`${apiUrl}?page=${currentPage}&size=${pageSize}`);
         const data = await response.json();
         renderTable(data);
-        //renderSummary(data);
     } catch (error) {
-         const summaryElement = document.getElementById("summary");
-            const count = data.length;
-            summaryElement.textContent = `You have ${count} stocks in your watchlist.`;
+        alert("Problem with the watchlist: "+error)
     }
 }
 
 function renderTable(watchList) {
+
     const tbody = document.querySelector('#watchlistTable tbody');
     tbody.innerHTML = '';
 
-    watchList.forEach((watchlist) => {
+    watchList.content.forEach((item) => {
         const tr = document.createElement('tr');
 
-        ['stockTickerInn', 'companyName', 'latestPrice', 'change_Price', 'change_Percentage', 'weeksRange','dividendYield', 'PE_Ratio', 'market_Cap'].forEach(key => {
+        const fields = ['stockTickerInn', 'companyName', 'latestPrice', 'change_Price', 'change_Percentage', 'weeksRange','dividendYield', 'PE_Ratio', 'market_Cap'];
+
+
+        fields.forEach(key => {
             const td = document.createElement('td');
-            let value = watchlist[key];
+            let value = item[key];
 
             if (key === 'weeksRange') {
-                value = `$${watchlist.low} - $${watchlist.High}`;
+                value = `$${item.low} - $${item.High}`;
             }
 
             if (['market_Cap'].includes(key)){
                 value = parseFloat(value);
 
                 if (key === 'market_Cap') {
-                   if (value >= 1e12) {
-                       value = `$${(value / 1e12).toFixed(2)}T`;
-                   } else if (value >= 1e9) {
-                       value = `$${(value / 1e9).toFixed(2)}B`;
-                   } else if (value >= 1e6) {
-                       value = `$${(value / 1e6).toFixed(2)}M`;
-                   } else if (value >= 1e3) {
-                      value = `$${(value / 1e3).toFixed(2)}K`;
-                   }
+                if (value >= 1e12) {
+                    value = `$${(value / 1e12).toFixed(2)}T`;
+                } else if (value >= 1e9) {
+                    value = `$${(value / 1e9).toFixed(2)}B`;
+                } else if (value >= 1e6) {
+                    value = `$${(value / 1e6).toFixed(2)}M`;
+                } else if (value >= 1e3) {
+                    value = `$${(value / 1e3).toFixed(2)}K`;
+                }
                 }
             } else if(['latestPrice', 'change_Price'].includes(key)){
                 value = `$${parseFloat(value).toFixed(2)}`;
@@ -66,12 +96,18 @@ function renderTable(watchList) {
 
         const actionTd = document.createElement('td');
         actionTd.innerHTML = `
-            <button class="delete-btn" onclick="deleteRow('${watchlist.id}')">Delete</button>
+            <button class="delete-btn" onclick="deleteRow('${item.id}')">Delete</button>
         `;
-        tr.dataset.id = watchlist.id;
+        tr.dataset.id = item.id;
         tr.appendChild(actionTd);
         tbody.appendChild(tr);
     });
+
+    pageInfo.textContent = `Page ${currentPage + 1} of ${watchList.totalPages}`;
+
+    backBtn.disabled = watchList.first;
+    nextBtn.disabled = watchList.last;
+    
 }
 
 function deleteRow(id) {
@@ -83,4 +119,3 @@ function deleteRow(id) {
 }
 
 fetchWatchlist();
-
